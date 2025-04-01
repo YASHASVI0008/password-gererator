4daf27fc2aa31f8e85990333a333873d539c1ca0
yashasvi tripathi <tripathiyashasvi123@gmail.com>
2025-04-02 02:08:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const passwordbox=document.getElementById("password");
+const length=12;
+const uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase="abcdefghijklmnopqrstuvwxyz";
+const number="0123456789";
+const symbol="@#$%!^&*(){}[]?|~";
+const allchar=uppercase+lowercase+number+symbol;
+function createpasswor ()
+{ 
+    let password="";
+    password+=uppercase[Math.floor(Math.random()*uppercase.length)];
+    password+=lowercase[Math.floor(Math.random()*lowercase.length)];
+    password+=number[Math.floor(Math.random()*number.length)];
+    password+=symbol[Math.floor(Math.random()*symbol.length)];
+
+    while(length>password.length)
+    {
+        password+=allchar[Math.floor(Math.random()*allchar.length)];
+    }
+    passwordbox.value=password;
+} 
+function copypassword()
+{
+    passwordbox.select();
+    document.execCommand("copy");
+}
